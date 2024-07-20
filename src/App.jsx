eeafa34a9fd525432016334622c117e587b14da8
Phiraveen <phiraveen.work@gmail.com>
2024-07-20 14:17:05 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Weather from './Weather'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function App() {
   
