b67d5b63135c744a887b5d78d0b6e478686f51b6
Phiraveen <phiraveen.work@gmail.com>
2024-07-20 11:57:17 +0530

    Added React bootstrap grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Weather from './Weather'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   
 
   return (
-    <>
+    
+    <Container>
+    <Row>
      <Weather />
-    </>
+    </Row>
+    </Container>
   )
 }
 
